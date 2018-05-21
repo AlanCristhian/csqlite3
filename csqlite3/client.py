9362c00d0f560de36e7c305943d155ee66922c46
unknown <alan.cristh@gmail.com>
2018-05-21 17:10:19 -0300

    Implement Cursor, register_adapter, register_converter and enable_callback_tracebacks.

--- csqlite3/client.py
+++ csqlite3/client.py
@@ -1,3 +1,4 @@
+# import atexit
 import logging
 import os
 import socket
@@ -15,10 +16,27 @@ class _ConnectionSocket(utils.PickleSocket):
         self.write(message)
         response = self.read()
         if isinstance(response, utils.ServerError):
-            raise response.error
+            raise Exception(response.error)
         elif isinstance(response, utils.ServerWarning):
             warnings.warn(response.warning.args[1], response.warning.__class__)
         return response
+
+
+class Cursor:
+    def __init__(self, socket):
+        self._socket = socket
+        self._socket.request(_PID, "cursor", "open", {})
+
+    def execute(self, sql, parameters=()):
+        args = (sql, parameters)
+        self._socket.request(_PID, "cursor", "execute", args)
+        return self
+
+    def fetchone(self):
+        return self._socket.request(_PID, "cursor", "fetchone", {})
+
+    def close(self):
+        return self._socket.request(_PID, "cursor", "close", {})
 
 
 class Connection:
@@ -48,9 +66,48 @@ class Connection:
         self._socket.request(_PID, "connection", "close", {})
         self._socket.close()
 
+    def cursor(self):
+        return Cursor(self._socket)
+
 
 def connect(database, timeout=5, detect_types=False, isolation_level="",
             check_same_thread=True, factory=Connection, cached_statements=100,
             uri=False):
     return factory(database, timeout, detect_types, isolation_level,
                    check_same_thread, cached_statements, uri)
+
+
+# @atexit.register
+# def close_client_app():
+#     with _ConnectionSocket(socket.AF_INET, socket.SOCK_STREAM) as _socket:
+#         _socket.settimeout(1/200)
+#         try:
+#             _socket.connect((utils.HOST, utils.PORT))
+#             _socket.request(_PID, "client_app", "close", {})
+#         except (ConnectionRefusedError, ConnectionResetError,
+#                 socket.timeout):
+#             pass
+
+
+def register_converter(typename, callable):
+    with _ConnectionSocket(socket.AF_INET, socket.SOCK_STREAM) as _socket:
+        _socket.settimeout(5)
+        _socket.connect((utils.HOST, utils.PORT))
+        args = (typename, callable)
+        _socket.request(_PID, "csqlite3", "register_converter", args)
+
+
+def register_adapter(type, callable):
+    with _ConnectionSocket(socket.AF_INET, socket.SOCK_STREAM) as _socket:
+        _socket.settimeout(5)
+        _socket.connect((utils.HOST, utils.PORT))
+        args = (type, callable)
+        _socket.request(_PID, "csqlite3", "register_adapter", args)
+
+
+def enable_callback_tracebacks(flag=False):
+    with _ConnectionSocket(socket.AF_INET, socket.SOCK_STREAM) as _socket:
+        _socket.settimeout(5)
+        _socket.connect((utils.HOST, utils.PORT))
+        _socket.request(_PID, "csqlite3", "enable_callback_tracebacks", [flag])
+
